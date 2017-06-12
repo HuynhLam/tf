9791b8402e59dc3a0ce12f9e12d0223bfa2b0d56
HuynhLam <lam.huynh0307@gmail.com>
2017-06-13 00:38:09 +0300

    Soft max func, udacity course

---
+++ udacity_softmax.py
@@ -0,0 +1,27 @@
+"""
+Udacity - Deep learning nano course
+Module 1
+Softmax.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    return np.exp(x)/np.sum(np.exp(x), axis=0)
+
+def main():
+    scores = [3.0, 1.0, 0.2]
+    print(softmax(scores))
+
+    # Plot softmax curves
+    x = np.arange(-2.0, 6.0, 0.1)
+    scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
+
+    plt.plot(x, softmax(scores).T, linewidth=2)
+    plt.show()
+    return
+
+if __name__ == "__main__":
+    main()
