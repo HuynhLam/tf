18899d5ae0b555744fa125ad413bbc32d5f0f6d2
HuynhLam <lam.huynh0307@gmail.com>
2017-06-13 01:22:01 +0300

    softmax *10 and /10 scores

--- udacity_softmax.py
+++ udacity_softmax.py
@@ -12,14 +12,16 @@ def softmax(x):
     return np.exp(x)/np.sum(np.exp(x), axis=0)
 
 def main():
-    scores = [3.0, 1.0, 0.2]
+    scores = np.array([3.0, 1.0, 0.2])
     print(softmax(scores))
+    print(softmax(scores * 10))
+    print(softmax(scores / 10))
 
     # Plot softmax curves
     x = np.arange(-2.0, 6.0, 0.1)
     scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
 
-    plt.plot(x, softmax(scores).T, linewidth=2)
+    plt.plot(x, softmax(scores / 10).T, linewidth=2)
     plt.show()
     return
 
