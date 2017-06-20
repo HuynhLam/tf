e86dead7e869d0dc89cccdbba6e5b45be12722c5
HuynhLam <lam.huynh0307@gmail.com>
2017-06-21 00:22:07 +0300

    change the cnn layer

--- simple_mnist_softmax.py
+++ simple_mnist_softmax.py
@@ -26,7 +26,7 @@ def main(_):
   y = tf.matmul(x, W) + b
 
   # Define loss and optimizer
-  y_ = tf.placeholder(tf.float32, [None, 10])
+  y_ = tf.placeholder(tf.float32, [None, 10])main()
 
   # The raw formulation of cross-entropy,
   #
@@ -48,7 +48,7 @@ def main(_):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
-  # Test trained model
+  # Test trained modelmain()
   correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
   accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
   print(sess.run(accuracy, feed_dict={x: mnist.test.images,
