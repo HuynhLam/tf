1a9dd66cd40c2baa9b1f8e4400af4f59c56ee18a
HuynhLam <lam.huynh0307@gmail.com>
2017-06-21 18:42:12 +0300

    MNIST keras, tf backend

--- mnist-board/mnist-board.py
+++ mnist-board/mnist-board.py
@@ -13,26 +13,26 @@ mnist = learn.datasets.mnist.read_data_sets("mnist", one_hot=True)
 
 
 def conv_layer(input, size_in, size_out, name="conv"):
-  with tf.name_scope(name):
-    w = tf.Variable(tf.truncated_normal([5, 5, size_in, size_out], stddev=0.1), name="W")
-    b = tf.Variable(tf.constant(0.1, shape=[size_out]), name="B")
-    conv = tf.nn.conv2d(input, w, strides=[1, 1, 1, 1], padding="SAME")
-    act = tf.nn.relu(conv + b)
-    tf.summary.histogram("weights", w)
-    tf.summary.histogram("biases", b)
-    tf.summary.histogram("activations", act)
-    return tf.nn.max_pool(act, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
+    with tf.name_scope(name):
+        w = tf.Variable(tf.truncated_normal([5, 5, size_in, size_out], stddev=0.1), name="W")
+        b = tf.Variable(tf.constant(0.1, shape=[size_out]), name="B")
+        conv = tf.nn.conv2d(input, w, strides=[1, 1, 1, 1], padding="SAME")
+        act = tf.nn.relu(conv + b)
+        tf.summary.histogram("weights", w)
+        tf.summary.histogram("biases", b)
+        tf.summary.histogram("activations", act)
+        return tf.nn.max_pool(act, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
 
 
 def fc_layer(input, size_in, size_out, name="fc"):
-  with tf.name_scope(name):
-    w = tf.Variable(tf.truncated_normal([size_in, size_out], stddev=0.1), name="W")
-    b = tf.Variable(tf.constant(0.1, shape=[size_out]), name="B")
-    act = tf.matmul(input, w) + b
-    tf.summary.histogram("weights", w)
-    tf.summary.histogram("biases", b)
-    tf.summary.histogram("activations", act)
-    return act
+    with tf.name_scope(name):
+        w = tf.Variable(tf.truncated_normal([size_in, size_out], stddev=0.1), name="W")
+        b = tf.Variable(tf.constant(0.1, shape=[size_out]), name="B")
+        act = tf.matmul(input, w) + b
+        tf.summary.histogram("weights", w)
+        tf.summary.histogram("biases", b)
+        tf.summary.histogram("activations", act)
+        return act
 
 
 def mnist_model(learning_rate, use_two_fc, use_two_conv, hparam):
@@ -137,4 +137,4 @@ def main():
     print("Done ! Lets see the board.")
 
 if __name__ == '__main__':
-  main()
+    main()
---
+++ test_keras.py
@@ -0,0 +1,59 @@
+''' Test karas on MNIST
+'''
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.datasets import mnist
+from keras.utils import np_utils
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+import os
+
+def main():
+    os.environ['CUDA_VISIBLE_DEVICES'] = "2"
+    # Import the MNIST data
+    #mnist = input_data.read_data_sets('mnist')
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+    # Preprocess input data
+    X_train = X_train.reshape(X_train.shape[0], 1, 28, 28)
+    X_test = X_test.reshape(X_test.shape[0], 1, 28, 28)
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+    X_train /= 255
+    X_test /= 255
+
+    # Preprocess class labels
+    Y_train = np_utils.to_categorical(y_train, 10)
+    Y_test = np_utils.to_categorical(y_test, 10)
+
+    model = Sequential()
+
+    model.add(Convolution2D(32, 3, 3, activation='relu', input_shape=(1,28,28), dim_ordering='th'))
+    model.add(Convolution2D(32, 3, 3, activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(10, activation='softmax'))
+
+    # Compile the model for use
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    # Training
+    print("******* Training: run 1 epochs... *******")
+    model.fit(X_train, Y_train, batch_size=32, nb_epoch=1, verbose=1)
+
+    # Evaluation
+    print("******* Testing *******")
+    loss_and_metrics = model.evaluate(x_test, y_test, batch_size=32)
+
+    return
+
+if __name__ == '__main__':
+    main()
